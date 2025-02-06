196725a149dc1343d30454c73a82ed5f2805f56d
Yordan Petrov <yordan.v.petrov@gmail.com>
2025-02-06 20:41:04 +0100

    Solutions.

--- JavaScript_Basics/01-First-Steps-In-Coding-Lab/yardGreening.js
+++
@@ -1,8 +0,0 @@
-function yardGreening(squareMeters) {
-    let totalPrice = squareMeters * 7.61;
-    let discount = totalPrice * 0.18;
-    let finalPrice = totalPrice - discount;
-    console.log(`The final price is: ${finalPrice} lv.`);
-    console.log(`The discount is: ${discount} lv.`);
-    
-}
